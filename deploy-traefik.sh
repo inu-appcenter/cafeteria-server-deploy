7319b6fdac0dc5fd24b9b37dde9ba74ae6360174
potados99 <potados99@gmail.com>
2021-10-02 23:26:35 +0900

    chore: 비밀번호 이스케이프

--- deploy-traefik.sh
+++ deploy-traefik.sh
@@ -5,6 +5,6 @@ export DOMAIN_MAIN=potados.com
 export DOMAIN_SANS=api.potados.com,console-api.potados.com
 
 export USERNAME=potados
-export HASHED_PASSWORD=$apr1$KK74D9PH$kmK1OiOdQlVXUF7SKLfkU.
+export HASHED_PASSWORD='$apr1$KK74D9PH$kmK1OiOdQlVXUF7SKLfkU.'
 
 docker stack deploy -c compose/traefik.yml traefik
