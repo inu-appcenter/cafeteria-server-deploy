6d51ffc4e6e4fdc4030519689783cca0b9f5eb19
potados99 <potados99@gmail.com>
2021-10-03 02:49:10 +0900

    chore: 도메인 변경

--- deploy-traefik.sh
+++ deploy-traefik.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export EMAIL=potados99@gmail.com
-export DOMAIN_MAIN=potados.com
-export DOMAIN_SANS=api.potados.com,console-api.potados.com
+export DOMAIN_MAIN=inu-cafeteria.app
+export DOMAIN_SANS=api.inu-cafeteria.app,console-api.inu-cafeteria.app
 
 export USERNAME=potados
 export HASHED_PASSWORD='$apr1$KK74D9PH$kmK1OiOdQlVXUF7SKLfkU.'
