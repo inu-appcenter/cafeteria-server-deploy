a761179bef497c3d4dabd10a6fab7446b87a11f9
potados99 <potados99@gmail.com>
2021-10-03 03:04:06 +0900

    chore: traefik.inu-cafeteria.app도 인증 범위에 추가

--- deploy-traefik.sh
+++ deploy-traefik.sh
@@ -2,7 +2,7 @@
 
 export EMAIL=potados99@gmail.com
 export DOMAIN_MAIN=inu-cafeteria.app
-export DOMAIN_SANS=api.inu-cafeteria.app,console-api.inu-cafeteria.app
+export DOMAIN_SANS=api.inu-cafeteria.app,console-api.inu-cafeteria.app,traefik.inu-cafeteria.app
 
 export USERNAME=potados
 export HASHED_PASSWORD='$apr1$KK74D9PH$kmK1OiOdQlVXUF7SKLfkU.'
